5cea3c5ad62f3079a5d4cf7196aca8576c80c0a4
refekt <39246154+refekt@users.noreply.github.com>
2019-07-12 10:19:33 -0400

    Added comments for readability

--- crystal_balls.py
+++ crystal_balls.py
@@ -11,36 +11,47 @@ def scrape_crystal_balls(year=0):
 
 
 def scrape_crystal_balls(year):
+    # Headers are requires to avoid the Interal Server Error (500) when using request.get()
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+    # URL for Steve Wiltfong's Crystal Ball predictdion history
     url = 'https://247sports.com/User/Steve-Wiltfong-73/Predictions/?Page=1&playerinstitution.primaryplayersport.recruitment.year={}&playerinstitution.primaryplayersport.sport=Football'.format(year)
+    
+    # Pull all data from the aforementioned URL
     page = requests.get(url=url, headers=headers)
+    # Convert into a BeautifulSoup4 object
     soup = BeautifulSoup(page.text, 'html.parser')
 
+    # Estbalishing variables for all desired data
+    # We are pulling data from the HTML element <li class='...'></li>
     main_photo = soup.find_all('li', class_='main-photo')
     name = soup.find_all('li', class_='name')
     predicted_by = soup.find_all('li', class_='predicted-by')
     prediction = soup.find_all('li', class_='prediction')
     correct = soup.find_all('li', class_='correct')
-    # pull every 'target' block
-    targets = soup.find_all('li', class_='target')
 
-    # This is a tough cookie. I'm going to loop over every target block we pulled and grab the info you want, then store in a nested dictionary, then add it to a list.
+    # This is a tough cookie. I'm going to loop over every target block we pulled and grab the info you want
+    # Then store in a nested dictionary, then add it to a list.
+    targets = soup.find_all('li', class_='target')
     for t in targets:
-        # for the current target block, grab the main photo url. I also split around ? and picked the first item from the result to get a url without the
-        # image width and height args
+        # for the current target block, grab the main photo url.
+        # I also split around ? and picked the first item from the result to get a url without the image width and height args
         main_photo = t.find(class_='main-photo').find('img').get('data-src').split('?')[0]
         # Grab the recruit's name. I'll also pull the profile url
+        # <li class='name'><a ...>FirstName LastName</a>
         name = t.find(class_='name').find('a').get_text()
+        # <li class='name'><a href='URL'>...</a>
         profile = t.find(class_='name').find('a').get('href')
 
         # I assume we want the percentages for each team from the predicted-by section. I'm going to do yet another dictionary lol
         teams = {}
         predicted_by = t.find(class_='predicted-by')
-
+        # If a recruit is commmited an <a> tag will be present with a nested <img> tag. 
         if predicted_by.find('a'):
             committed_team = predicted_by.find('a').find('img').get('alt')
             teams[committed_team] = "Committed"
+        # Predictions are stored in <li> elements
         else:
+            # Change predicted_by from <li class='predicted-by'> to a list of all <li> elements within <li class='predicted-by'>
             predicted_by = predicted_by.find_all('li')
             for p in predicted_by:
                 # team name
@@ -52,9 +63,8 @@ def scrape_crystal_balls(year):
                 p_percent = p.find('span').get_text()
                 teams[p_team] = p_percent
 
-        # Should I dump all the contents of crystal_balls into a new string to make a new soup?
-        # To search for sub items?
-        # since we are looking at a free page, the prediction can differ if its a locked pick or free pick. I'm going to first check if it's locked or not and handle it from there
+        # Since we are looking at a free page, the prediction can differ if its a locked pick or free pick.
+        # I'm going to first check if it's locked or not and handle it from there
         unlock = t.find(class_='prediction').find('div').find('div')
         # I noticed locked picks are two nested divs while free picks are one div containing a team image
         pick = 'VIP'
@@ -68,11 +78,6 @@ def scrape_crystal_balls(year):
             correct_incorrect = result_exist.find('g').find('circle')
             if correct_incorrect:
                 correct = 'Incorrect'
-
-
-        """crystal_balls = soup.find_all(class_='target')
-        for x in range(len(crystal_balls)):
-            print(crystal_balls[x])"""
         # YES!!! WOOO!
         prediction_time = t.find(class_='prediction-date')
         prediction_time = prediction_time.get_text().strip()
@@ -87,7 +92,15 @@ def scrape_crystal_balls(year):
                     'Result': correct
                     }
         cb_list.append(sub_dict)
-
+        
+        # Now for the real work. Steve Wiltfong's historical predictions need to be gathered and stored.
+        # Last checked there was 14 pages. This should only have to be pulled one time and then saved.
+        # When pulling new CBs, we will pull the first page and compare that dict/list, sub_dict, against
+        # the JSON file for identical matches. If a non-match is found, it'll have to be added to the 
+        # JSON and sorted by prediction_time if possible. If not, reverse the JSON and append the list, dict, 
+        # or JSON and add all together for historical data.
+        
+    # Dumps cb_list into a JSON file
     with open('crystal_balls.json', 'w') as fp:
         json.dump(cb_list, fp, sort_keys=True, indent=4)
 
