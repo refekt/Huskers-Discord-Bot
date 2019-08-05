17bf646c77661af5461205471a98c62a2a220b0e
refekt <refekt@gmail.com>
2019-08-04 21:11:12 -0400

    Small tweak to $crootbot

--- cogs/croot_bot.py
+++ cogs/croot_bot.py
@@ -287,6 +287,7 @@ class CrootBot(commands.Cog, name="Croot Bot"):
             twitter_raw = str(twitter_raw[0])
             twitter_raw2 = re.findall(re_pattern, twitter_raw)
             twitter_handle = twitter_raw2[0]
+            twitter_handle = twitter_handle.replace("@","")
             twitter_handle = "@" + twitter_handle[15:-1]
             twitter_url = "https://twitter.com/" + twitter_handle
         else:
