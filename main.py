d47fe578905faecf39e50e500c0daddb9bdf509c
refekt <refekt@gmail.com>
2019-08-12 16:29:59 -0400

    stuff

--- main.py
+++ main.py
@@ -122,6 +122,12 @@ async def on_message(message):
             await message.add_reaction(emojiUpvote)
             await message.add_reaction(emojiDownvote)
 
+        # Reply to NotA_Virus
+        notavirus = "NotaVirus_Click#3411"
+        if message.author == notavirus:
+            if "you suck" in message.content.lower():
+                await message.channel.send("HEY NOW! I am on to you {}...".format(message.author))
+
     # HUDL highlight puller on react. This section is to take the crootbot message, find if a hudl profile exists, and pull the video. 
     # Next would be to monitor reactions and post the video if someone reacted to the video camera emoji.
     # TODO If there are multiple football players with the same name we may get the wrong guy. Especially for croots from previous classes. We will want to add more logic to narrow 
