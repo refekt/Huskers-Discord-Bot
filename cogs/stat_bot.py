fdfc3af51b385ed1852db6848100071b342c0e10
refekt <refekt@gmail.com>
2019-09-04 21:07:14 -0400

    Update stat_bot.py

--- cogs/stat_bot.py
+++ cogs/stat_bot.py
@@ -45,7 +45,7 @@ class StatBot(commands.Cog, name="CFB Stats"):
         game_datetime_utc = pytz.utc.localize(game_datetime_raw)
         game_datetime_cst = game_datetime_utc.astimezone(pytz.timezone("America/Chicago"))
 
-        embed.add_field(name="Most Recent Match Up", value="Date: {}\nLocation: {}\n{} : {} - {} :{}\n".format(
+        embed.add_field(name="Most Recent Match Up", value="Date: {}\nLocation: {}\n{} : {} - {} : {}\n".format(
             game_datetime_cst,
             matchup_json["games"][gameHistLen]["venue"],
             matchup_json["games"][gameHistLen]["homeTeam"],
