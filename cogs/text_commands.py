fef8a2c5617794d77ebe17073087f1e2b067c204
refekt <refekt@gmail.com>
2019-09-11 17:39:07 -0400

    Update text_commands.py

--- cogs/text_commands.py
+++ cogs/text_commands.py
@@ -33,17 +33,15 @@ class TextCommands(commands.Cog, name="Text Commands"):
                 if msg.content != "" and not msg.author.bot:
                     source_data += msg.content + ". "
         else:
-            async for msg in ctx.channel.history(limit=5000):
-                if msg.content != "" and str(msg.author) == str(user) and not msg.author.bot:
-                    source_data += msg.content + ". "
+            try:
+                async for msg in ctx.channel.history(limit=5000):
+                    if msg.content != "" and str(msg.author) == str(user) and not msg.author.bot:
+                        source_data += msg.content + ". "
+            except:
+                await edit_msg.edit(content="⚠ Something went wrong. You broke me! ⚠")
 
         chain = markovify.Text(source_data, well_formed=True)
         sentence = chain.make_sentence(tries=100, max_chars=60, max_overlap_ratio=.78)
-
-        # Get rid of things that would be annoying
-        sentence.replace("$","_")
-        sentence.replace("@","~")
-        sentence.replace("..", ".)")
 
         await edit_msg.edit(content=sentence)
 
