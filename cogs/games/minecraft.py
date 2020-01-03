266feff0114630bff3c54564a49e467b62657b7d
refekt <refekt@gmail.com>
2020-01-02 22:36:46 -0500

    Updated `minecraft.py`; added `minecraft status`

--- cogs/games/minecraft.py
+++ cogs/games/minecraft.py
@@ -1,57 +1,68 @@
 from discord.ext import commands
 
-# from utils.consts import admin_prod, admin_test
+from utils.consts import role_admin_prod, role_admin_test
 from utils.consts import ssh_pw, ssh_user, ssh_host
 from utils.embed import build_embed
 
+from paramiko import client as p_client
 
-# from paramiko import client as client
+ssh_commands = {
+    "status": "ps aux | grep spigot",
+    "free": "free"
+}
 
-# ssh_commands = {
-#     "status": "ps aux | grep spigot"
-# }
+
+class ssh:
+    client = None
+
+    def __init__(self, address, username, password):
+        print("^^^ Connecting to server... ^^^")
+
+        self.client = p_client.SSHClient()
+        self.client.set_missing_host_key_policy(p_client.AutoAddPolicy())
+        self.client.connect(address, username=username, password=password, look_for_keys=False)
+
+    def sendCommand(self, command):
+        print(f"^^^ Sending command [{command}] ^^^")
+
+        import select
+
+        alldata = ""
+
+        if self.client:
+            stdin, stdout, stderr = self.client.exec_command(command)
+
+            while not stdout.channel.exit_status_ready():
+                rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
+
+                if len(rl) > 0:
+                    alldata += str(stdout.channel.recv(1024), "utf-8")
+                    break
+
+            return f"{alldata if alldata else '*'}"
+        else:
+            print("^^^ Connection not opened! ^^^")
+
+    def cleanup(self):
+        print("^^^ Closing SSH server connection... ^^^")
+        self.client.close()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cleanup()
 
 
-# class ssh:
-#     client = None
-#
-#     def __init__(self, address, username, password):
-#         print("^^^ Connecting to server... ^^^")
-#
-#         self.client = client.SSHClient()
-#         self.client.set_missing_host_key_policy(client.AutoAddPolicy())
-#         self.client.connect(address, username=username, password=password, look_for_keys=False)
-#
-#     def sendCommand(self, command):
-#         print(f"^^^ Sending command [{command}] ^^^")
-#
-#         import select
-#
-#         alldata = "```\n"
-#
-#         if self.client:
-#             stdin, stdout, stderr = self.client.exec_command(command)
-#
-#             while not stdout.channel.exit_status_ready():
-#                 rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
-#
-#                 if len(rl) > 0:
-#                     alldata += str(stdout.channel.recv(1024), "utf-8")
-#                     break
-#
-#             alldata += "\n```"
-#
-#             return f"All Data: " \
-#                    f"{alldata if alldata else '*'}"
-#         else:
-#             print("^^^ Connection not opened! ^^^")
-#
-#     def cleanup(self):
-#         print("^^^ Closing SSH server connection... ^^^")
-#         self.client.close()
-#
-#     def __exit__(self, exc_type, exc_val, exc_tb):
-#         self.cleanup()
+def format_data(data: str, filter: str=""):
+    split = data.split("\n")
+    output = ""
+
+    for s in split:
+        if filter:
+            if not filter in s:
+                continue
+        if s:
+            output += f"`{s}`\n"
+
+    return output
 
 
 def connect_SSH():
@@ -63,19 +74,33 @@ class MinecraftCommands(commands.Cog, name="Minecraft Commands"):
     async def minecraft(self, ctx):
         pass
 
-    # @commands.has_any_role(admin_prod, admin_test)
-    # @minecraft.command()
-    # async def status(self, ctx):
-    #     ssh = connect_SSH()
-    #     await ctx.send(ssh.sendCommand(ssh_commands["status"]))
-    #     del ssh
+    @commands.has_any_role(role_admin_prod, role_admin_test)
+    @minecraft.command()
+    async def status(self, ctx):
+        edit_msg = await ctx.send("Loading...")
+        ssh = connect_SSH()
+        check = True
+        data = ""
+        while check:
+            data = ssh.sendCommand(ssh_commands["status"])
+            if "minecra" in data:
+                check = False
+
+        data = format_data(data, "java")
+        await edit_msg.edit(content=data)
+        del ssh
 
     @minecraft.command()
     async def server(self, ctx):
         await ctx.send(
             embed=build_embed(
                 title="Husker Discord Minecraft Server",
-                fields=[["Version", "Java"], ["Server", "202.5.24.139"], ["Port", "25565"], ["Dynamic Map", "http://202.5.24.139:8123/"]]
+                fields=[
+                    ["Version", "Java"],
+                    ["Server", "202.5.24.139"],
+                    ["Port", "25565"],
+                    ["Dynamic Map", "http://202.5.24.139:8123/"]
+                ]
             )
         )
 
