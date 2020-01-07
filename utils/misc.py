1b7ddad5edad818149e38d60a58624a1957d2036
refekt <refekt@gmail.com>
2020-01-07 11:52:41 -0500

    Updated MD5 generator

--- utils/misc.py
+++ utils/misc.py
@@ -1,3 +1,4 @@
+import hashlib
 import sys
 
 from unidecode import unidecode
@@ -14,3 +15,16 @@ def bot_latency():
 
 def on_prod_server():
     return True if sys.argv[1] == "prod" else False
+
+
+def makeMD5():
+    names = ["389886063453405205"]
+    animals = ["lemur"]
+    mammals = dict()
+    i = 0
+    for name in names:
+        hash_object = hashlib.md5(name.encode())
+        mammals.update({animals[i]: hash_object.hexdigest()})
+        i += 1
+
+    print(mammals)
