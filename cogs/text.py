2332d42e5bcf39fef3aa809769ce6376a6365895
refekt <refekt@gmail.com>
2020-01-08 21:56:50 -0500

    Added `police` command

--- cogs/text.py
+++ cogs/text.py
@@ -291,7 +291,7 @@ class TextCommands(commands.Cog):
     @commands.command()
     @commands.cooldown(rate=_global_rate, per=_global_per, type=_global_type)
     async def police(self, ctx, baddie: discord.Member):
-        await ctx.send(f"{baddie.mention}❕❔Information ℹ provided in the VIP 👑 Room 🏆 is intended for Husker247 🌽🎈 members only ‼🔫. "
+        await ctx.send(f"🚨🚨🚨 **NANI** {baddie.mention}❕❔Information ℹ provided in the VIP 👑 Room 🏆 is intended for Husker247 🌽🎈 members only ‼🔫. "
                        f"Please do not copy ✏ and paste 🖨 or summarize this content elsewhere‼ Please try to keep all replies in this thread 🧵 for Husker247 members only! "
                        f"🚫 ⛔ 👎 🙅‍♀️Thanks for your cooperation. 😍🤩😘")
 
