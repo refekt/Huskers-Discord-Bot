d516280878de4c93b9e66078a7af1f2ab07ad1e9
refekt <refekt@gmail.com>
2020-02-22 09:11:53 -0500

    Updated functionality for chat bot

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -1,6 +1,7 @@
 from chatterbot.chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from discord.ext import commands
+from utils.consts import ROLE_ADMIN_TEST, ROLE_ADMIN_PROD, ROLE_MOD_PROD
 
 chatbot = ChatBot("Bot Frost", logic_adapters=["chatterbot.logic.BestMatch", "chatterbot.logic.MathematicalEvaluation"])
 trainer = ChatterBotCorpusTrainer(chatbot)
@@ -8,14 +9,16 @@ trainer = ChatterBotCorpusTrainer(chatbot)
 
 class ChatBot(commands.Cog):
     @commands.command()
+    @commands.has_any_role(ROLE_MOD_PROD, ROLE_ADMIN_PROD, ROLE_ADMIN_TEST)
     async def cb_train(self, ctx):
         edit_msg = await ctx.send("Training myself...")
         trainer = ChatterBotCorpusTrainer(chatbot)
-        trainer.train("chatterbot.corpus.english")
+        trainer.train("chatterbot.corpus.english", "chatterbot.corpus.english.greetings", "chatterbot.corpus.english.conversations")
 
         await edit_msg.edit(content="Chat bot trained!")
 
     @commands.command()
+    @commands.has_any_role(ROLE_MOD_PROD, ROLE_ADMIN_PROD, ROLE_ADMIN_TEST)
     async def cb_export(self, ctx):
         trainer.export_for_training("export.json")
 
