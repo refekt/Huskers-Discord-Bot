cb04fd806ad863a9ace7a6de3586031b74ad9156
refekt <refekt@gmail.com>
2020-02-21 19:50:27 -0500

    Testing chat bot

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -1,5 +1,5 @@
 from chatterbot.chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 from discord.ext import commands
 
 chatbot = ChatBot("Bot Frost Bot")
@@ -8,8 +8,16 @@ chatbot = ChatBot("Bot Frost Bot")
 def convert_messages_to_list(messages):
     output_list = []
 
+    def cleanup_message(msg: str):
+        msg = msg.replace("`","")
+        msg = msg.replace("\n", "")
+
     for message in messages:
-        output_list.append(message.clean_content)
+        if message.clean_content and not message.clean_content == "\n":
+            output_list.append(cleanup_message(message.clean_content))
+
+    from pprint import pprint
+    pprint(output_list)
 
     return output_list
 
@@ -31,6 +39,13 @@ class ChatBot(commands.Cog):
     @commands.command()
     async def cb_say(self, ctx, *, question):
         await ctx.send(chatbot.get_response(question))
+
+    @commands.command()
+    async def cb_train(self, ctx):
+        trainer = ListTrainer(chatbot)
+        messages = await ctx.channel.history().flatten()
+        list = convert_messages_to_list(messages)
+        trainer.train(list)
 
 
 def setup(bot):
