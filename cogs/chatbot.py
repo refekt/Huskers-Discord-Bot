72fa4dd6b146c76fd16bbbfc3d8f7d57dc8cf4c2
refekt <refekt@gmail.com>
2020-02-21 20:21:47 -0500

    Testing chat bot

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -1,7 +1,7 @@
 from chatterbot.chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from discord.ext import commands
-from utils.client import client
+
 
 chatbot = ChatBot("Bot Frost")
 trainer = ListTrainer(chatbot)
@@ -20,7 +20,7 @@ class ChatBot(commands.Cog):
             return msg
 
         for msg in message_list:
-            if msg.clean_content and not msg.clean_content.startswith(client.command_prefix):
+            if msg.clean_content and not msg.clean_content.startswith("$"):
                 training_list.append(cleanup_message(msg.clean_content))
 
         trainer.train(training_list)
