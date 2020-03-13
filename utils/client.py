f94271aaa574cafb97523f28dd27b3da13f366ff
refekt <refekt@gmail.com>
2020-03-12 20:41:23 -0400

    Fixed auto replies

--- utils/client.py
+++ utils/client.py
@@ -252,8 +252,9 @@ async def monitor_messages(message: discord.Message):
                 else:
                     await message.channel.send("I don't understand. We can try again next time!")
 
-    if not message.author.bot and message.channel.id not in CHAN_BANNED:
-        await auto_replies()
+    if not message.author.bot:
+        if message.channel.id not in CHAN_BANNED:
+            await auto_replies()
         await find_subreddits()
         await add_votes()
 
