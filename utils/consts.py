dabbad8933233ebe4eb99f1012430f7c291ec27b
refekt <refekt@gmail.com>
2020-08-14 16:40:29 -0400

    Added encryption to environment variables

--- utils/consts.py
+++ utils/consts.py
@@ -1,3 +1,4 @@
+from utils.misc import decrypt, load_key, encrypt, write_key, decrypt_return_data
 import os
 import platform
 import random
@@ -14,48 +15,55 @@ print(f"### Platform == {platform.platform()} ###")
 
 if "Windows" in platform.platform():
     print("### Windows environment set ###")
-    load_dotenv(dotenv_path="variables.env")
+    load_dotenv(dotenv_path="variables.json")
 elif "Linux" in platform.platform():
     if on_prod_server():
         print("### Production environment set ###")
-        load_dotenv(dotenv_path="/home/botfrost/bot/variables.env")
+        load_dotenv(dotenv_path="/home/botfrost/bot/variables.json")
     elif not on_prod_server():
         print("### Test environment set ###")
-        load_dotenv(dotenv_path="/home/botfrost/bot/variables.env")
+        load_dotenv(dotenv_path="/home/botfrost/bot/variables.json")
 else:
     print(f"Unknown Platform: {platform.platform()}")
 
+# Decrypt Env file
+env_file = "variables.json"
+key = load_key()
+env_vars = decrypt_return_data(env_file, key)
+
 # Cooldown rates for commands
-CD_GLOBAL_RATE = os.getenv("global_rate")
-CD_GLOBAL_PER = os.getenv("global_per")
+CD_GLOBAL_RATE = env_vars["global_rate"]
+CD_GLOBAL_PER = env_vars["global_per"]
 CD_GLOBAL_TYPE = BucketType.user
 
 # Discord Bot Tokens
-TEST_TOKEN = os.getenv("TEST_TOKEN")
-PROD_TOKEN = os.getenv("DISCORD_TOKEN")
-BACKUP_TOKEN = os.getenv("BACKUP_TOKEN")
+TEST_TOKEN = env_vars["TEST_TOKEN"]
+PROD_TOKEN = env_vars["DISCORD_TOKEN"]
+BACKUP_TOKEN = env_vars["BACKUP_TOKEN"]
 
 # SQL information
-SQL_HOST = os.getenv("sqlHost")
-SQL_USER = os.getenv("sqlUser")
-SQL_PASSWD = os.getenv("sqlPass")
-SQL_DB = os.getenv("sqlDb")
+SQL_HOST = env_vars["sqlHost"]
+SQL_USER = env_vars["sqlUser"]
+SQL_PASSWD = env_vars["sqlPass"]
+SQL_DB = env_vars["sqlDb"]
 
 # Reddit Bot Info
-REDDIT_CLIENT_ID = os.getenv("reddit_client_id")
-REDDIT_SECRET = os.getenv("reddit_secret")
-REDDIT_PW = os.getenv("reddit_pw")
+REDDIT_CLIENT_ID = env_vars["reddit_client_id"]
+REDDIT_SECRET = env_vars["reddit_secret"]
+REDDIT_PW = env_vars["reddit_pw"]
 
 # SSH Information
-SSH_HOST = os.getenv("ssh_host")
-SSH_USER = os.getenv("ssh_user")
-SSH_PW = os.getenv("ssh_pw")
+SSH_HOST = env_vars["ssh_host"]
+SSH_USER = env_vars["ssh_user"]
+SSH_PW = env_vars["ssh_pw"]
 
 # Twitter variables
-TWITTER_CONSUMER_KEY = os.getenv("twitter_consumer_key")
-TWITTER_CONSUMER_SECRET = os.getenv("twitter_consumer_secret")
-TWITTER_TOKEN_KEY = os.getenv("twitter_token_key")
-TWITTER_TOKEN_SECRET = os.getenv("twitter_token_secret")
+TWITTER_CONSUMER_KEY = env_vars["twitter_consumer_key"]
+TWITTER_CONSUMER_SECRET = env_vars["twitter_consumer_secret"]
+TWITTER_TOKEN_KEY = env_vars["twitter_token_key"]
+TWITTER_TOKEN_SECRET = env_vars["twitter_token_secret"]
+
+del env_vars, env_file, key
 
 # Headers for `requests`
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'}
