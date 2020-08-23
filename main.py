f8eb5e65169fa9e4df0db0bfa0b51112d3092271
refekt <refekt@gmail.com>
2020-08-23 11:02:11 -0400

    Changed logging level

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 import logging
 import utils.client  # This is required because it loads everything else
 
-logging.basicConfig(filename="logging.log", level=logging.INFO)
+logging.basicConfig(filename="logging.log", level=logging.WARNING)
