3e6661098be342dfed537bfe9d3a9f0edfba5897
refekt <refekt@gmail.com>
2020-10-02 15:11:34 -0400

    Update git ignore

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python3.7
-import logging
-import utils.client  # This is required because it loads everything else
 
-logging.basicConfig(filename="logging.log", level=logging.WARNING)
+import utils.client  # This is required because it loads everything else
