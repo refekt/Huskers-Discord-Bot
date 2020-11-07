0464e62ca018eaa21d180d7d74238d3f60316ef6
refekt <refekt@gmail.com>
2020-11-07 16:23:16 -0500

    Fixed resolved

--- cogs/betting.py
+++ cogs/betting.py
@@ -906,7 +906,7 @@ class BetCommands(commands.Cog, name="Betting Commands"):
 
         author = ctx.guild.get_member(original_bet["author"])
 
-        if not original_bet["author"] == ctx.message.author.id or ROLE_ADMIN_PROD in [role.id for role in ctx.message.author.roles]:
+        if ROLE_ADMIN_PROD not in [role.id for role in ctx.message.author.roles] or not original_bet["author"] == ctx.message.author.id:
             raise AttributeError(f"You cannot update a bet you did not create! The original author for [ {keyword} ] is [ {author.mention} ]. ")
 
         try:
