720398d85305395bb06ed62fe2ec5a4f09f78ccf
refekt <refekt@gmail.com>
2020-11-28 10:58:51 -0500

    Bug fix

--- cogs/images.py
+++ cogs/images.py
@@ -264,13 +264,13 @@ class ImageCommands(commands.Cog, name="Fun Image Commands"):
     @commands.cooldown(rate=CD_GLOBAL_RATE, per=CD_GLOBAL_PER, type=CD_GLOBAL_TYPE)
     async def redname(self, ctx):
         """ Hi Redname """
-        await ctx.send(embed=build_image_embed(title="Hi Redname", image="https://gph.is/2pLAfqc"))
+        await ctx.send(embed=build_image_embed(title="Hi Redname", image="https://i.imgur.com/XFPqoT5.gif"))
         
     @commands.command()
     @commands.cooldown(rate=CD_GLOBAL_RATE, per=CD_GLOBAL_PER, type=CD_GLOBAL_TYPE)
     async def bigmacksack(self, ctx):
         """ Big Mac Sack """
-        await ctx.send(embed=build_image_embed(title="BigMacSack", image="https://imgur.com/a/9XP8Zad"))
+        await ctx.send(embed=build_image_embed(title="BigMacSack", image="https://i.imgur.com/XUdlxBe.gif"))
 
 def setup(bot):
     bot.add_cog(ImageCommands(bot))
