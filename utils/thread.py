0b8cb9e249c7e56677835bfe873b4f3a915db1ac
refekt <refekt@gmail.com>
2021-02-13 15:26:12 -0500

    Fixed reminders

--- utils/thread.py
+++ utils/thread.py
@@ -38,21 +38,21 @@ def remove_mentions(message):
 #             print("Slept!")
 
 
-async def send_reminder(thread, duration, who: typing.Union[discord.Member, discord.TextChannel], message, author: typing.Union[discord.Member, discord.TextChannel], flag=None):
+async def send_reminder(thread, duration, who: typing.Union[discord.Member, discord.TextChannel], message, author: typing.Union[discord.Member, discord.TextChannel], alert_when=None):
     if exitFlag:
         thread.exit()
 
-    print(f"### ;;; Starting [{thread}] thread for [{duration}] seconds. Send_When == [{flag}].")
+    print(f"### ;;; Starting [{thread}] thread for [{duration}] seconds. Send_When == [{alert_when}].")
 
     if duration > 0:
         print(f"### ;;; Creating a task for [{duration}] seconds. [{who}] [{message[:15] + '...'}]")
         await asyncio.sleep(duration)
         await who.send(f"[Reminder from [{author}] for {who}]: {remove_mentions(message)}")
-        process_MySQL(sqlUpdateTasks, values=(0, who.id, message, str(flag), author))
+        process_MySQL(sqlUpdateTasks, values=(0, who.id, message, alert_when, str(author)))
     else:
-        imported_datetime = datetime.strptime(flag, "%Y-%m-%d %H:%M:%S.%f")
+        imported_datetime = datetime.strptime(alert_when, "%Y-%m-%d %H:%M:%S.%f")
         await who.send(f"[Missed reminder from [{author}] for [{who}] set for [{imported_datetime.strftime('%x %X')}]!: {remove_mentions(message)}")
-        process_MySQL(sqlUpdateTasks, values=(0, who.id, message, str(flag), author))
+        process_MySQL(sqlUpdateTasks, values=(0, who.id, message, alert_when, str(author)))
 
     print(f"### ;;; Thread [{thread}] completed successfully!")
 
