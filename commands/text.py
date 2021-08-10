b40abf3b70d2aebd3358721b56578ab81f7c8528
refekt <refekt@gmail.com>
2021-08-09 20:46:17 -0400

    Estbalishing `markov`

--- commands/text.py
+++ commands/text.py
@@ -1,13 +1,19 @@
+import random
+import re
 from urllib import parse
 
+import discord
+import markovify
 import requests
 from bs4 import BeautifulSoup
 from discord.ext import commands
 from discord_slash import ButtonStyle, ComponentContext
 from discord_slash import cog_ext
 from discord_slash.context import SlashContext
+from discord_slash.utils.manage_commands import create_option
 from discord_slash.utils.manage_components import create_button, create_actionrow
 
+from utilities.constants import CHAN_BANNED
 from utilities.constants import command_error
 from utilities.embed import build_embed
 from utilities.server_detection import which_guild
@@ -206,6 +212,106 @@ class TextCommands(commands.Cog):
                 await ctx.edit_origin(content="", embed=embed, components=[create_actionrow(*buttons_voting)])
             except KeyError:
                 raise command_error("Error modifying Down Votes")
+
+    def check_channel_or_message(self, check_member: discord.Member, check_message: discord.Message = None):
+        if check_message.content == "":
+            return ""
+
+        if check_message.channel.id in CHAN_BANNED:
+            return ""
+
+        if check_member.bot:
+            return ""
+
+        return "\n" + str(check_message.content.capitalize())
+
+    def cleanup_source_data(self, source_data: str):
+        regex_strings = [
+            r"(<@\d{18}>|<@!\d{18}>|<:\w{1,}:\d{18}>|<#\d{18}>)",  # All Discord mentions
+            r"((Http|Https|http|ftp|https)://|)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"  # All URLs
+        ]
+
+        new_source_data = source_data
+
+        for regex in regex_strings:
+            new_source_data = re.sub(regex, "", new_source_data, flags=re.IGNORECASE)
+
+        regex_new_line = r"(\r\n|\r|\n){1,}"  # All new lines
+        new_source_data = re.sub(regex_new_line, "\n", new_source_data, flags=re.IGNORECASE)
+
+        regex_front_new_line = r"^\n"
+        new_source_data = re.sub(regex_front_new_line, "", new_source_data, flags=re.IGNORECASE)
+
+        regex_multiple_whitespace = r"\s{2,}"
+        new_source_data = re.sub(regex_multiple_whitespace, " ", new_source_data, flags=re.IGNORECASE)
+
+        return new_source_data
+
+    @cog_ext.cog_slash(
+        name="markov",
+        description="Attempts to create a meaningful sentence from old messages",
+        guild_ids=[which_guild()],
+        options=[
+            create_option(
+                name="channel",
+                description="Discord text channel",
+                option_type=7,
+                required=False
+            ),
+            create_option(
+                name="member",
+                description="Discord member",
+                option_type=6,
+                required=False
+            )
+        ]
+    )
+    async def _markov(self, ctx: SlashContext, channel=None, member=None):
+        await ctx.defer()
+
+        sources = []
+        if channel is not None:
+            sources.append(channel)
+
+        if member is not None:
+            sources.append(member)
+
+        source_data = ""
+
+        compiled_message_history = []
+
+        CHAN_HIST_LIMIT = 1000
+
+        if not sources:  # Uses current channel for source data
+            compiled_message_history = await ctx.channel.history(limit=CHAN_HIST_LIMIT).flatten()  # potential discord vs discord_slash issue
+            for message in compiled_message_history:
+                source_data += self.check_channel_or_message(ctx.author, message)
+        else:
+            for source in sources:
+                if type(source) == discord.Member:  # Use current channel and source Discord Member
+                    compiled_message_history = await ctx.channel.history(limit=CHAN_HIST_LIMIT).flatten()
+                    for message in compiled_message_history:
+                        if message.author == source:
+                            source_data += self.check_channel_or_message(message.author, message)
+                elif type(source) == discord.TextChannel:
+                    compiled_message_history = await source.history(limit=CHAN_HIST_LIMIT).flatten()
+                    for message in compiled_message_history:
+                        source_data += self.check_channel_or_message(message.author, message)
+
+        if not source_data == "":
+            source_data = self.cleanup_source_data(source_data)
+        else:
+            await ctx.send(f"There was not enough information available to make a Markov chain.")
+
+        chain = markovify.NewlineText(source_data, well_formed=True)
+        markov_output = chain.make_sentence(max_overlap_ratio=.9, max_overlap_total=27, min_words=7, tries=100)
+
+        if markov_output is None:
+            await ctx.send(f"Creating a Markov chain failed.")
+        else:
+            punctuation = ("!", ".", "?", "...")
+            markov_output += random.choice(punctuation)
+            await ctx.send(markov_output)
 
 
 def setup(bot):
