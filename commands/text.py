2e64c8879f6b30c341d44e04f020cd6423433bbd
refekt <refekt@gmail.com>
2021-08-15 11:56:35 -0400

    Display tweak for `urbandictionary`

--- commands/text.py
+++ commands/text.py
@@ -6,17 +6,19 @@ import discord
 import markovify
 import requests
 from bs4 import BeautifulSoup
+from dinteractions_Paginator import Paginator
 from discord.ext import commands
-from discord_slash import ButtonStyle, ComponentContext
+from discord_slash import ComponentContext
 from discord_slash import cog_ext
 from discord_slash.context import SlashContext
+from discord_slash.model import ButtonStyle
 from discord_slash.utils.manage_commands import create_option
 from discord_slash.utils.manage_components import create_button, create_actionrow
 
 from utilities.constants import CHAN_BANNED, CHAN_POSSUMS
 from utilities.constants import command_error
-from utilities.embed import build_embed
 from utilities.constants import which_guild
+from utilities.embed import build_embed
 
 buttons_ud = [
     create_button(
@@ -44,9 +46,6 @@ buttons_voting = [
         custom_id="vote_down"
     )
 ]
-
-results = []
-result_index = 0
 
 
 def ud_embed(embed_word, embed_meaning, embed_example, embed_contributor):
@@ -89,6 +88,7 @@ class TextCommands(commands.Cog):
 
         del r, soup, definitions[1]  # Word of the day
 
+        results = []
         for definition in definitions:
             results.append(self.Definition(
                 lookup_word=definition.contents[1].contents[0].text,
@@ -97,52 +97,30 @@ class TextCommands(commands.Cog):
                 contributor=definition.contents[4].text
             ))
 
-        global result_index
-        result_index = 0
+        pages = []
+        for index, result in enumerate(results):
+            pages.append(build_embed(
+                title=f"Searched for: {result.lookup_word}",
+                description=f"Definition #{index + 1} from Urban Dictionary",
+                fields=[
+                    ["Meaning", result.meaning],
+                    ["Example", result.example],
+                    ["Contributor", result.contributor]
+                ]
+            ))
 
-        embed = ud_embed(
-            embed_word=results[result_index].lookup_word,
-            embed_meaning=results[result_index].meaning,
-            embed_example=results[result_index].example,
-            embed_contributor=results[result_index].contributor
+        await Paginator(
+            bot=ctx.bot,
+            ctx=ctx,
+            pages=pages,
+            useIndexButton=True,
+            useSelect=False,
+            firstStyle=ButtonStyle.gray,
+            nextStyle=ButtonStyle.gray,
+            prevStyle=ButtonStyle.gray,
+            lastStyle=ButtonStyle.gray,
+            indexStyle=ButtonStyle.gray
         )
-
-        actionrow_ud = create_actionrow(*buttons_ud)
-        await ctx.send(embed=embed, components=[actionrow_ud])
-
-    @cog_ext.cog_component(components=buttons_ud)
-    async def process_ud_buttons(self, ctx: ComponentContext):
-        global results, result_index
-
-        if ctx.custom_id == "ud_previous":
-            if result_index == 0:
-                return
-            result_index -= 1
-        elif ctx.custom_id == "ud_next":
-            if result_index == len(results):
-                return
-            result_index += 1
-
-        if 0 < result_index < (len(results) - 1):
-            buttons_ud[0]["disabled"] = False
-            buttons_ud[1]["disabled"] = False
-
-        else:
-            if result_index <= 0:
-                buttons_ud[0]["disabled"] = True
-            elif result_index >= len(results) - 1:
-                buttons_ud[1]["disabled"] = True
-
-        actionrow_ud = create_actionrow(*buttons_ud)
-
-        edited_embed = ud_embed(
-            embed_word=results[result_index].lookup_word,
-            embed_example=results[result_index].example,
-            embed_meaning=results[result_index].meaning,
-            embed_contributor=results[result_index].contributor
-        )
-
-        await ctx.edit_origin(content="", embed=edited_embed, components=[actionrow_ud])
 
     @cog_ext.cog_slash(
         name="vote",
