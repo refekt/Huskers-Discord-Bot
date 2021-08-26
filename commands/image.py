637f855405789afc757479536e6ca0e790bae5fc
refekt <refekt@gmail.com>
2021-08-26 14:05:29 -0400

    Adjusting Paginator

--- commands/image.py
+++ commands/image.py
@@ -181,8 +181,7 @@ class ImageCommands(commands.Cog):
             prevStyle=ButtonStyle.gray,
             lastStyle=ButtonStyle.gray,
             indexStyle=ButtonStyle.gray,
-            hidden=True,
-            dm=True
+            hidden=True
         ).run()
 
     @cog_ext.cog_slash(
