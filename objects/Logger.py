3a18ab42b94b649e3aca93276d1db17ea37bef65
refekt <refekt@gmail.com>
2022-06-22 13:43:57 -0400

    Updated logs to be smaller file size and have a lot per python file

--- objects/Logger.py
+++ objects/Logger.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import pathlib
-import platform
 import sys
 
 __all__ = ["discordLogger"]
@@ -10,13 +9,13 @@ from logging.handlers import RotatingFileHandler
 
 
 def discordLogger(name: str, level: int = logging.INFO) -> logging.Logger:
-    path = pathlib.Path("bot.log")
-    full_path = os.path.join(path.parent.resolve(), path)
+    path = pathlib.Path(f"{__name__}.log")
+    full_path = os.path.join(path.parent.resolve(), "logs", path)
 
     format_string = "[%(asctime)s] %(levelname)s :: %(name)s :: %(module)s :: func/%(funcName)s :: Ln/%(lineno)d :: %(message)s"
     formatter = logging.Formatter(format_string)
 
-    handler = RotatingFileHandler(filename=full_path, mode="a", maxBytes=5242880)
+    handler = RotatingFileHandler(filename=full_path, mode="a", maxBytes=2_621_440)
     handler.setFormatter(formatter)
     handler.setLevel(logging.INFO)
 
