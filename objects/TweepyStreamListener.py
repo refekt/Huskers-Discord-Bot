b2f989557e7f838f01c0cad2b1266527b15f90d3
refekt <refekt@gmail.com>
2022-07-06 11:11:19 -0400

    Type hinting and __slots__

--- objects/TweepyStreamListener.py
+++ objects/TweepyStreamListener.py
@@ -1,11 +1,13 @@
 import asyncio
 import json
 import time
+from asyncio import Future
 from pprint import pprint
 from typing import Union, Optional
 
 import dateutil.parser
 import discord
+import requests
 import tweepy
 from tweepy import Response
 
@@ -16,13 +18,14 @@ from helpers.constants import (
     CHAN_TWITTERVERSE,
     DEBUGGING_CODE,
     TWITTER_BLOCK16_SCREENANME,
+    CHAN_BOT_SPAM_PRIVATE,
 )
 from helpers.embed import buildEmbed, buildTweetEmbed
 from objects.Logger import discordLogger
 
 logger = discordLogger(__name__)
 
-__all__ = ["StreamClientV2"]
+__all__: list[str] = ["StreamClientV2"]
 
 
 # Example
@@ -33,54 +36,71 @@ __all__ = ["StreamClientV2"]
 
 
 class MyTweet(object):
+    __slots__ = [
+        "data",
+        "includes",
+        "matching_rules",
+    ]  # TODO Verify there are not any more variables
+    
     def __init__(self, tweet_data) -> None:
-        self.data = None
-        self.includes = None
-        self.matching_rules = None
-
         for key in tweet_data:
             setattr(self, key, tweet_data[key])
 
 
 class TweetMediaData(object):
+    __slots__ = ["url"]  # TODO Verify there are not any more variables
+    
     def __init__(self, tweet_data) -> None:
-        self.url = None
-
         for key in tweet_data:
             setattr(self, key, tweet_data[key])
 
 
 class TweetQuoteData(object):
+    __slots__ = [
+        "author_id",
+        "id",
+        "text",
+    ]  # TODO Verify there are not any more variables
+    
     def __init__(self, tweet_data) -> None:
-        self.id = None
-        self.text = None
-        self.author_id = None
         for key in tweet_data:
             setattr(self, key, tweet_data[key])
 
 
 class TweetUserData(object):
+    __slots__ = [
+        "created_at",
+        "description",
+        "followers",
+        "following",
+        "id",
+        "listed_count",
+        "name",
+        "profile_image_url",
+        "protected",
+        "public_metrics",
+        "tweet_count",
+        "url",
+        "username",
+        "verified",
+    ]
+    
     def __init__(self, data) -> None:
-        self.public_metrics = None
-        self.verified = None
-        self.profile_image_url = None
-        self.name = None
-        self.username = None
-        self.followers = None
-        self.following = None
-        self.tweet_count = None
-        self.listed_count = None
         for key in data:
             setattr(self, key, data[key])
 
 
 async def send_tweet_alert(client: discord.Client, message) -> None:
     if DEBUGGING_CODE:
-        logger.debug("Skipping tweet alert because debugging is on")
-        return
-
+        twitter_channel: discord.TextChannel = await client.fetch_channel(
+            CHAN_BOT_SPAM_PRIVATE
+        )
+    
+    else:
+        twitter_channel = await client.fetch_channel(CHAN_TWITTERVERSE)
+    
     logger.debug(f"Tweet alert received: {message}")
-    embed = buildEmbed(
+    embed: discord.Embed = buildEmbed(
         title="Husker Twitter",
         fields=[
             dict(
@@ -89,28 +109,35 @@ async def send_tweet_alert(client: discord.Client, message) -> None:
             )
         ],
     )
-
-    twitter_channel: discord.TextChannel = await client.fetch_channel(CHAN_TWITTERVERSE)
+    
     await twitter_channel.send(embed=embed)
-
+    
     logger.info(f"Twitter alert sent!")
 
 
 async def send_tweet(client: discord.Client, tweet: MyTweet) -> None:
     class TwitterButtons(discord.ui.View):
+        __slots__ = [
+            "children",
+            "id",
+            "message",
+            "timeout",
+        ]
+        
         def __init__(self, timeout=1200) -> None:
             super(TwitterButtons, self).__init__()
             self.message: Optional[discord.Message, None] = None
             self.timeout = timeout
-
+        
+        # noinspection PyMethodMayBeStatic
         def grabTwitterLink(self, tweet_embed: discord.Embed) -> str:
-            link = [
+            link: list[str] = [
                 field.value
                 for field in tweet_embed.fields
                 if field.name == "Link to Tweet"
             ]
             return "".join(link)
-
+        
         @discord.ui.button(
             label="Send to General",
             custom_id="send_to_general",
@@ -120,12 +147,12 @@ async def send_tweet(client: discord.Client, tweet: MyTweet) -> None:
             self, interaction: discord.Interaction, button: discord.ui.Button
         ):
             logger.debug("Sending tweet to general channel")
-            chan = await client.fetch_channel(CHAN_GENERAL)
+            chan: discord.TextChannel = await client.fetch_channel(CHAN_GENERAL)
             await chan.send(
                 f"{interaction.user.name}#{interaction.user.discriminator} forwarded the following tweet: {self.grabTwitterLink(interaction.message.embeds[0])}"
             )
             await interaction.response.send_message("Tweet forwarded!", ephemeral=True)
-
+        
         @discord.ui.button(
             label="Send to Recruiting",
             custom_id="send_to_recruiting",
@@ -135,41 +162,52 @@ async def send_tweet(client: discord.Client, tweet: MyTweet) -> None:
             self, interaction: discord.Interaction, button: discord.ui.Button
         ):
             logger.debug("Sending tweet to recruiting channel")
-            chan = await client.fetch_channel(CHAN_RECRUITING)
+            chan: discord.TextChannel = await client.fetch_channel(CHAN_RECRUITING)
             await chan.send(
                 f"{interaction.user.name}#{interaction.user.discriminator} forwarded the following tweet: {self.grabTwitterLink(interaction.message.embeds[0])}"
             )
             await interaction.response.send_message("Tweet forwarded!", ephemeral=True)
-
+        
         async def on_timeout(self) -> None:
             logger.debug("Twitter buttons have timed out. Removing options")
             self.clear_items()
             await self.message.edit(view=self)
-
+        
         async def callback(self, interaction: discord.Interaction) -> None:
             pass
-
+    
     logger.debug(f"Sending a tweet")
-
-    author = None  # noqa
+    
+    if DEBUGGING_CODE:
+        twitter_channel: discord.TextChannel = await client.fetch_channel(
+            CHAN_BOT_SPAM_PRIVATE
+        )
+        food_channel: discord.TextChannel = await client.fetch_channel(
+            CHAN_BOT_SPAM_PRIVATE
+        )
+    else:
+        twitter_channel = await client.fetch_channel(CHAN_TWITTERVERSE)
+        food_channel = await client.fetch_channel(CHAN_FOOD)
+    
+    author: Optional[TweetUserData] = None
     for user in tweet.includes["users"]:
         if tweet.data["author_id"] == user["id"]:
-            author: TweetUserData = TweetUserData(user)
+            author = TweetUserData(user)
             break
-
-    medias = []
+    
+    medias: list[TweetMediaData] = []
     if "media" in tweet.includes:
         for item in tweet.includes["media"]:
             medias.append(TweetMediaData(item))
-
-    quotes = []
+    
+    quotes: list[TweetQuoteData] = []
     if "tweets" in tweet.includes:
         for item in tweet.includes["tweets"]:
             quotes.append(TweetQuoteData(item))
-
+    
     if author.username.lower() == TWITTER_BLOCK16_SCREENANME.lower():
         logger.info("Sending a Block 16 twweet")
-        embed = buildTweetEmbed(
+        embed: discord.Embed = buildTweetEmbed(
             name=author.name,
             username=author.username,
             author_metrics=author.public_metrics,
@@ -185,12 +223,11 @@ async def send_tweet(client: discord.Client, tweet: MyTweet) -> None:
             quotes=quotes,
             b16=True,
         )
-
-        food_channel: discord.TextChannel = await client.fetch_channel(CHAN_FOOD)
+        
         if food_channel:
             await food_channel.send(embed=embed)
         return
-
+    
     embed = buildTweetEmbed(
         name=author.name,
         username=author.username,
@@ -206,8 +243,8 @@ async def send_tweet(client: discord.Client, tweet: MyTweet) -> None:
         medias=medias,
         quotes=quotes,
     )
-
-    view = TwitterButtons()
+    
+    view: TwitterButtons = TwitterButtons()
     view.add_item(
         item=discord.ui.Button(
             style=discord.ButtonStyle.url,
@@ -215,14 +252,16 @@ async def send_tweet(client: discord.Client, tweet: MyTweet) -> None:
             url=f"https://twitter.com/{author.username}/status/{tweet.data['id']}",
         )
     )
-    twitter_channel: discord.TextChannel = await client.fetch_channel(CHAN_TWITTERVERSE)
+    
     view.message = await twitter_channel.send(embed=embed, view=view)
     await view.wait()
-
+    
     logger.info(f"Tweet sent!")
 
 
 class StreamClientV2(tweepy.StreamingClient):
+    __slots__ = ["client", "cooldown", "bearer_token"]
+    
     def __init__(
         self,
         bearer_token,
@@ -232,82 +271,82 @@ class StreamClientV2(tweepy.StreamingClient):
         super().__init__(bearer_token, **kwargs)
         self.client = client
         self.cooldown = 5
-
+    
     def remove_all_rules(self) -> None:
         raw_rules: Union[dict, Response, Response] = self.get_rules()
         if raw_rules.data is not None:
-            ids = [rule.id for rule in raw_rules.data]
+            ids: list[int] = [rule.id for rule in raw_rules.data]
             self.delete_rules(ids)
-
+    
     def on_connect(self) -> None:
         logger.debug("Connected!")
-
+        
         self.cooldown = 5
-
-        raw_rules = self.get_rules()
+        
+        raw_rules: Union[dict, Response, requests.Response] = self.get_rules()
         auths: Union[str, list[str]] = ""
-
+        
         if raw_rules.data is not None:
             for rule in raw_rules.data:
                 auths += rule.value + " OR "
-
+            
             auths = auths[:-4]
             auths = auths.replace("from:", "@")
             auths = auths.split(" OR ")
             auths = ", ".join(auths)
-
-        task = asyncio.run_coroutine_threadsafe(
+        
+        task: Future = asyncio.run_coroutine_threadsafe(
             send_tweet_alert(self.client, f"Connected! Following: {auths}"),
             self.client.loop,
         )
         task.result()
-
+    
     def on_request_error(self, status_code) -> None:
         logger.error(f"Request Error: {status_code}")
-
+        
         logger.debug(
             f"Sleeping for {self.cooldown} seconds before attempting to reconnected"
         )
         time.sleep(self.cooldown)
         self.cooldown = min(self.cooldown * 2, 900)  # Max of 900 seconds or 15 minutes
         logger.debug("Sleep is over")
-
+    
     def on_connection_error(self) -> None:
         logger.error(f"Connection Error")
-        task = asyncio.run_coroutine_threadsafe(
+        task: Future = asyncio.run_coroutine_threadsafe(
             send_tweet_alert(
                 self.client, "The Twitter Stream had an error connecting!"
             ),
             self.client.loop,
         )
         task.result()
-
+    
     def on_disconnect(self) -> None:
         logger.warning("Disconnected")
-        task = asyncio.run_coroutine_threadsafe(
+        task: Future = asyncio.run_coroutine_threadsafe(
             send_tweet_alert(self.client, "The Twitter Stream has been disconnected!"),
             self.client.loop,
         )
         task.result()
-
+    
     def on_errors(self, errors) -> None:
         logger.error(f"Error received: {errors}")
-
+    
     def on_closed(self, response) -> None:
         logger.warning(f"Closed: {response}")
-
+    
     def on_exception(self, exception) -> None:
         logger.error(f"Exception: {exception}")
-
+    
     def on_data(self, raw_data) -> None:
         logger.debug(pprint(json.loads(raw_data)))
-
-        processed_data = json.loads(raw_data)
+        
+        processed_data: dict = json.loads(raw_data)
         task = asyncio.run_coroutine_threadsafe(
             send_tweet(self.client, MyTweet(processed_data)), self.client.loop
         )
         task.result()
-
+    
     def on_keep_alive(self) -> None:
         logger.debug("Keep Alive signal received")
 
