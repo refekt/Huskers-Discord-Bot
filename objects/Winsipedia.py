b9a493a6cfd300123eed71ef88a4155fa24585b1
refekt <refekt@gmail.com>
2022-07-06 11:30:14 -0400

    Type hinting

--- objects/Winsipedia.py
+++ objects/Winsipedia.py
@@ -1,5 +1,8 @@
+from typing import Union
+
 import requests
 from bs4 import BeautifulSoup
+from bs4.element import ResultSet
 
 from helpers.constants import HEADERS
 from objects.Exceptions import StatsException
@@ -7,13 +10,17 @@ from objects.Logger import discordLogger
 
 logger = discordLogger(__name__)
 
-__all__ = ["TeamStatsWinsipediaTeam", "CompareWinsipediaTeam", "CompareWinsipedia"]
+__all__: list[str] = [
+    "TeamStatsWinsipediaTeam",
+    "CompareWinsipediaTeam",
+    "CompareWinsipedia",
+]
 
 
 class TeamStatsWinsipediaTeam:
     def __init__(self, *, team_name: str) -> None:
-        def all_time_record():
-            atr = soup.find_all(attrs={"class": "ranking span2 item2"})
+        def all_time_record() -> Union[tuple[str, str], str]:
+            atr: ResultSet = soup.find_all(attrs={"class": "ranking span2 item2"})
             try:
                 return (
                     atr[0].contents[1].contents[3].contents[1].text,
@@ -22,8 +29,8 @@ class TeamStatsWinsipediaTeam:
             except:  # noqa
                 return "UNK", "UNK"
 
-        def championships():
-            champs = soup.find_all(attrs={"class": "ranking span2 item3"})
+        def championships() -> Union[tuple[str, str], str]:
+            champs: ResultSet = soup.find_all(attrs={"class": "ranking span2 item3"})
             try:
                 return (
                     champs[0].contents[3].contents[1].text,
@@ -32,8 +39,8 @@ class TeamStatsWinsipediaTeam:
             except:  # noqa
                 return "UNK", "UNK"
 
-        def conf_championships():
-            conf = soup.find_all(attrs={"class": "ranking span2 item4h"})
+        def conf_championships() -> Union[tuple[str, str], str]:
+            conf: ResultSet = soup.find_all(attrs={"class": "ranking span2 item4h"})
             try:
                 return (
                     conf[0].contents[3].contents[1].text,
@@ -43,7 +50,7 @@ class TeamStatsWinsipediaTeam:
                 return "UNK", "UNK"
 
         def bowl_games():
-            bowl = soup.find_all(attrs={"class": "ranking span2 item5h"})
+            bowl: ResultSet = soup.find_all(attrs={"class": "ranking span2 item5h"})
             try:
                 return (
                     bowl[0].contents[1].contents[1].text,
@@ -52,8 +59,8 @@ class TeamStatsWinsipediaTeam:
             except:  # noqa
                 return "UNK", "UNK"
 
-        def all_time_wins():
-            atw = soup.find_all(attrs={"class": "ranking span2 item1"})
+        def all_time_wins() -> Union[tuple[str, str], str]:
+            atw: ResultSet = soup.find_all(attrs={"class": "ranking span2 item1"})
             try:
                 return (
                     atw[0].contents[1].contents[1].text,
@@ -62,8 +69,8 @@ class TeamStatsWinsipediaTeam:
             except:  # noqa
                 return "UNK", "UNK"
 
-        def bowl_record():
-            bowl = soup.find_all(attrs={"class": "ranking span2 item2"})
+        def bowl_record() -> Union[tuple[str, str], str]:
+            bowl: ResultSet = soup.find_all(attrs={"class": "ranking span2 item2"})
             try:
                 return (
                     bowl[1].contents[1].contents[3].contents[1].text,  # \n and \t
@@ -72,8 +79,8 @@ class TeamStatsWinsipediaTeam:
             except:  # noqa
                 return "UNK", "UNK"
 
-        def consensus_all_americans():
-            caa = soup.find_all(attrs={"class": "ranking span2 item3"})
+        def consensus_all_americans() -> Union[tuple[str, str], str]:
+            caa: ResultSet = soup.find_all(attrs={"class": "ranking span2 item3"})
             try:
                 return (
                     caa[1].contents[1].contents[1].text,
@@ -82,8 +89,8 @@ class TeamStatsWinsipediaTeam:
             except:  # noqa
                 return "UNK", "UNK"
 
-        def heisman_winners():
-            hw = soup.find_all(attrs={"class": "ranking span2 item4"})
+        def heisman_winners() -> Union[tuple[str, str], str]:
+            hw: ResultSet = soup.find_all(attrs={"class": "ranking span2 item4"})
             try:
                 return (
                     hw[1].contents[3].contents[1].text,
@@ -92,8 +99,8 @@ class TeamStatsWinsipediaTeam:
             except:  # noqa
                 return "UNK", "UNK"
 
-        def nfl_draft_picks():
-            nfl_picks = soup.find_all(attrs={"class": "ranking span2 item5"})
+        def nfl_draft_picks() -> Union[tuple[str, str], str]:
+            nfl_picks: ResultSet = soup.find_all(attrs={"class": "ranking span2 item5"})
             try:
                 return (
                     nfl_picks[1].contents[3].contents[1].text,
@@ -102,8 +109,8 @@ class TeamStatsWinsipediaTeam:
             except:  # noqa
                 return "UNK", "UNK"
 
-        def weeks_ap_poll():
-            ap_poll = soup.find_all(attrs={"class": "ranking span2 item6"})
+        def weeks_ap_poll() -> Union[tuple[str, str], str]:
+            ap_poll: ResultSet = soup.find_all(attrs={"class": "ranking span2 item6"})
             try:
                 return (
                     ap_poll[0].contents[3].contents[1].text,
@@ -112,11 +119,11 @@ class TeamStatsWinsipediaTeam:
             except:  # noqa
                 return "UNK", "UNK"
 
-        self.team_name = team_name.replace(" ", "-").replace("&", "")
-        self.url = f"http://www.winsipedia.com/{self.team_name}"
+        self.team_name: str = team_name.replace(" ", "-").replace("&", "")
+        self.url: str = f"http://www.winsipedia.com/{self.team_name}"
 
-        re = requests.get(url=self.url, headers=HEADERS)
-        soup = BeautifulSoup(re.content, features="html.parser")
+        re: requests.Response = requests.get(url=self.url, headers=HEADERS)
+        soup: BeautifulSoup = BeautifulSoup(re.content, features="html.parser")
 
         self.all_time_record = all_time_record()[0]
         self.all_time_record_rank = all_time_record()[1]
@@ -141,7 +148,13 @@ class TeamStatsWinsipediaTeam:
 
 
 class CompareWinsipediaTeam:
-    name = None
+    __slots__ = [
+        "largest_mov",
+        "largest_mov_date",
+        "longest_win_streak",
+        "longest_win_streak_date",
+        "name",
+    ]
 
     def __init__(
         self,
@@ -151,32 +164,42 @@ class CompareWinsipediaTeam:
         longest_win_streak,
         largest_win_streak_date,
     ):
-        self.name = name
         self.largest_mov = largest_mov
         self.largest_mov_date = largest_mov_date
         self.longest_win_streak = longest_win_streak
-        self.largest_win_streak_date = largest_win_streak_date
+        self.longest_win_streak_date = largest_win_streak_date
+        self.name = name
 
 
 class CompareWinsipedia:
+    __slots__ = ["url", "full_games_url", "all_time_record", "compare", "against"]
+
     def __init__(self, compare: str, against: str) -> None:
-        self.url = f"http://www.winsipedia.com/{compare.lower()}/vs/{against.lower()}"
-        self.full_games_url = (
+        self.url: str = (
+            f"http://www.winsipedia.com/{compare.lower()}/vs/{against.lower()}"
+        )
+        self.full_games_url: str = (
             f"http://www.winsipedia.com/games/{compare.lower()}/vs/{against.lower()}"
         )
 
-        def mov(which: int) -> list:
-            raw_mov = soup.find_all(attrs={"class": f"ranking span2 item{which}"})
+        def mov(which: int) -> list[str]:
+            raw_mov: Union[ResultSet, str] = soup.find_all(
+                attrs={"class": f"ranking span2 item{which}"}
+            )
             raw_mov = raw_mov[0].contents[3].text.replace("\n \n", ":").strip()
             return raw_mov.split(":")
 
-        def win(which: int) -> list:
-            raw_win = soup.find_all(attrs={"class": f"ranking span2 item{which}"})
+        def win(which: int) -> list[str]:
+            raw_win: Union[ResultSet, str] = soup.find_all(
+                attrs={"class": f"ranking span2 item{which}"}
+            )
             raw_win = raw_win[0].contents[3].text.replace("\n \n", ":").strip()
             return raw_win.split(":")
 
         def all_time_wins() -> str:
-            wins = soup.find_all(attrs={"class": "titleItem left"})[0].contents[1].text
+            wins: ResultSet = (
+                soup.find_all(attrs={"class": "titleItem left"})[0].contents[1].text
+            )
             ties = (
                 soup.find_all(attrs={"class": "titleItem center"})[0].contents[1].text
             )
@@ -190,17 +213,17 @@ class CompareWinsipedia:
             compare.replace(" ", "").isalpha() and against.replace(" ", "").isalpha()
         ), StatsException("Team names must only contain alphabet letters.")
 
-        re = requests.get(url=self.url, headers=HEADERS)
-        soup = BeautifulSoup(re.content, features="html.parser")
+        re: requests.Response = requests.get(url=self.url, headers=HEADERS)
+        soup: BeautifulSoup = BeautifulSoup(re.content, features="html.parser")
 
         assert not re.status_code == 404, StatsException(
             f"Unable to find provided teams: {compare}, {against}"
         )
 
-        margin_of_victory = mov(1)
-        win_stream = win(2)
-        self.all_time_record = all_time_wins()
-        self.compare = CompareWinsipediaTeam(
+        margin_of_victory: list[str] = mov(1)
+        win_stream: list[str] = win(2)
+        self.all_time_record: str = all_time_wins()
+        self.compare: CompareWinsipediaTeam = CompareWinsipediaTeam(
             name=compare,
             largest_mov=margin_of_victory[0],
             largest_mov_date=margin_of_victory[1],
@@ -208,9 +231,9 @@ class CompareWinsipedia:
             largest_win_streak_date=win_stream[1],
         )
 
-        margin_of_victory = mov(6)
-        win_stream = win(5)
-        self.against = CompareWinsipediaTeam(
+        margin_of_victory: list[str] = mov(6)
+        win_stream: list[str] = win(5)
+        self.against: CompareWinsipediaTeam = CompareWinsipediaTeam(
             name=compare,
             largest_mov=margin_of_victory[0],
             largest_mov_date=margin_of_victory[1],
